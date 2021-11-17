6e94a4cc379ac9e142795c22cc2a96e4da1efb6a
Aleksey <jetta261@gmail.com>
2021-11-17 12:37:38 +0300

    1

--- application/iterator.py
+++ application/iterator.py
@@ -7,22 +7,29 @@ from data.data import nested_list
 class MyIterator:
 
     def __init__(self, colors) -> None:
-        self.colors = colors
-        self.i = -1
+        self.colors = sum(colors, [])
+        
 
     def __iter__(self):
+        self.i = -1
         return self
+        
 
     def __next__(self):
+        
         self.i += 1
         if self.i == len(self.colors):
             raise StopIteration
+        # while self.colors:
+
         if isinstance(self.colors, list):
+           
             return self.colors[self.i]
 
 
 # for i in MyIterator(nested_list):
-#     for j in i:
+    # print(i)
+    # for j in i:
 #         print(j)
 
 
