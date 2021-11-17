3063f1e6c137a09366afa3883510bb0bc533b156
Aleksey <jetta261@gmail.com>
2021-11-17 12:37:28 +0300

    1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import List
 from application.generator import non_recursive_generator, recursive_generator
 from application.iterator import MyIterator
 from data.data import nested_list, nested_list_1
@@ -5,8 +6,8 @@ from data.data import nested_list, nested_list_1
 
 print("=" * 30)
 for i in MyIterator(nested_list):
-    for j in i:
-        print(j)
+    print(i)
+    
 
 
 print("=" * 30)
@@ -17,3 +18,10 @@ for i in non_recursive_generator(nested_list):
 print("=" * 30)
 for i in recursive_generator(nested_list_1):
     print(i)
+
+
+# for row in nested_list:
+#     for elem in row:
+#         print(elem, end = ' ')
+#     print()
+
